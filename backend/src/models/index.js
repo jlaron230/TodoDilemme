776d6eb8374ac2c942ef5d9bd970199c7b41f957
Mehdy <Medoubouzid0@gmail.com>
2024-09-13 01:40:22 +0200

    fix/resolve conflict

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -1,9 +1,11 @@
 require("dotenv").config();
 
-// db.js
-const mysql = require('mysql2/promise');
+const mysql = require("mysql2/promise");
 
-// Créer un pool de connexions pour éviter de créer plusieurs connexions
+// create a connection pool to the database
+
+const { DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME } = process.env;
+
 const pool = mysql.createPool({
   host: DB_HOST,
   port: DB_PORT,
@@ -12,17 +14,16 @@ const pool = mysql.createPool({
   database: DB_NAME,
 });
 
-module.exports = pool; // Exporter le pool pour l'utiliser dans d'autres fichiers
+// try a connection
 
-// Tester la connexion à la base de données
-async function testConnection() {
-  try {
-    await pool.query('SELECT 1');
-    console.log('Connexion à la base de données réussie!');
-  } catch (error) {
-    console.error('Erreur lors de la connexion à la base de données:', error);
-  }
-}
+pool.getConnection().catch(() => {
+  console.warn(
+    "Warning:",
+    "Failed to get a DB connection.",
+    "Did you create a .env file with valid credentials?",
+    "Routes using models won't work as intended"
+  );
+});
 
 testConnection(); // Appeler la fonction pour tester la connexion
 
