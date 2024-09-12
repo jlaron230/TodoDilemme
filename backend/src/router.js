33e3ddda67bf7c6d98a0a770bf685a5b1fe70138
Mehdy <Medoubouzid0@gmail.com>
2024-09-13 01:31:22 +0200

    fix/resolv conflict + correctly connect BDD

--- backend/src/router.js
+++ backend/src/router.js
@@ -2,11 +2,11 @@ const express = require("express");
 
 const router = express.Router(); // Create an instance of Express router
 
-/*router.get("/candidacies", candidacyControllers.browse);
+router.get("/candidacies", candidacyControllers.browse);
 router.get("/candidacies/:id", candidacyControllers.read);
 router.put("/candidacies/:id", candidacyControllers.edit);
 router.post("/candidacies", candidacyControllers.add);
-router.delete("/candidacies/:id", candidacyControllers.destroy);*/
+router.delete("/candidacies/:id", candidacyControllers.destroy);
 
 module.exports = router;
 
