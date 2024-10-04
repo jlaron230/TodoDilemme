002850793077fc7e91a7b06fb2bc4ed16d081675
Medou13500 <medoubouzid0@gmail.com>
2024-10-05 00:16:08 +0200

    feat/backend modification

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,8 +1,10 @@
 const express = require("express");
 
+
 const router = express.Router(); // Create an instance of Express router
 
 const candidacyControllers = require("./controllers/candidacyControllers");
+const UserController= require("./controllers/userControllers");
 
 router.get("/candidacies", candidacyControllers.browse);
 router.get("/candidacies/:id", candidacyControllers.read);
@@ -10,17 +12,17 @@ router.put("/candidacies/:id", candidacyControllers.edit);
 router.post("/candidacies", candidacyControllers.add);
 router.delete("/candidacies/:id", candidacyControllers.destroy);
 
-// Associer les routes aux contrôleurs
+/* // Associer les routes aux contrôleurs
 router.get("/task-cat", exports.getAllTaskCat);
 router.post("/task-cat", exports.createTaskCat);
 router.put("/task-cat/:id", exports.updateTaskCat);
-router.delete("/task-cat/:id", exports.deleteTaskCat);
+router.delete("/task-cat/:id", exports.deleteTaskCat); */
 
 
-router.get("/user", exports.getAllUsers);
-router.post("/user", exports.CreateUser);
-router.put("/user/:id", exports.updateUser);
-router.delete("/user/:id", exports.DeleteUser);
+router.get("/user", UserController.getAllUsers);
+router.post("/user", UserController.createUser);
+router.put("/user/:id", UserController.updateUser);
+router.delete("/user/:id", UserController.deleteUser);
 
 
 module.exports = router;
